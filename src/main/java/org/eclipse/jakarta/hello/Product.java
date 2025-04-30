f7e989f8209cd544744fb1b23c79eac2b33e972a
Shahdd21 <88803131+Shahdd21@users.noreply.github.com>
2025-04-30 14:26:56 +0300

    simple CRUD operations

---
+++ src/main/java/org/eclipse/jakarta/hello/Product.java
@@ -0,0 +1,36 @@
+package org.eclipse.jakarta.hello;
+
+public class Product {
+
+    private String name;
+    private Integer price;
+
+    public Product(String name, Integer price) {
+        this.price = price;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                '}' + '\n';
+    }
+}
---
+++ src/main/java/org/eclipse/jakarta/hello/ProductsResource.java
@@ -0,0 +1,71 @@
+package org.eclipse.jakarta.hello;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet("/products")
+public class ProductsResource extends HttpServlet {
+
+    Map<String, Product> products = new HashMap<>(Map.of(
+            "Shaan body milk", new Product("Shaan body milk", 200),
+            "Eva lotion", new Product("Eva lotion", 150),
+            "Care and More cream", new Product("Care and More cream", 50),
+            "Pure cream", new Product("Pure cream", 20)
+    ) );
+
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        PrintWriter out = response.getWriter();
+
+        out.println(products.values());
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        response.setContentType("text/html");
+
+        String name = request.getParameter("name");
+        int price = Integer.parseInt(request.getParameter("price"));
+
+        Product product = new Product(name, price);
+        products.put(name, product);
+
+        response.getWriter().println("<p> Product is added successfully ! </p>");
+    }
+
+    public void doPut(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        response.setContentType("text/html");
+
+        String name = request.getParameter("name");
+        int price = Integer.parseInt(request.getParameter("price"));
+
+        Product product = products.get(name);
+        product.setPrice(price);
+
+        products.put(name, product);
+
+        response.getWriter().println("<p> Product is updated successfully ! </p>");
+    }
+
+    public void doDelete(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        response.setContentType("text/html");
+
+        String name = request.getParameter("name");
+
+        products.remove(name);
+
+        response.getWriter().println("<p> Product is deleted successfully ! </p>");
+    }
+
+}
+
